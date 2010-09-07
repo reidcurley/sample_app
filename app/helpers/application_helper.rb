b55f1f1f4961d7082cfb7929f8e0ac8e55482fc1
Reid Curley <rcurley@ruxtonpartners.com>
2010-09-06 20:31:55 -0400

    Through Ch 9 - Done w sign in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 module ApplicationHelper
 
   def logo
-    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
   end
 
   # Return a title on a per-page basis.
