b6ac86517171c5100d409bb431d437064d68d1a5
Reid Curley <rcurley@ruxtonpartners.com>
2010-09-11 17:01:46 -0400

    Thru Ch 10 - Done w user edit/update, index, destroy

--- config/environment.rb
+++ config/environment.rb
@@ -16,6 +16,7 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on and have them installed with rake gems:install
   config.gem 'factory_girl'
+  config.gem 'will_paginate', :version => '2.3.12'
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
